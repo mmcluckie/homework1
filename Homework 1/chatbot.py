213057cdb47d026af49057a1409e311c25e43472
mmcluckie <127161725+mmcluckie@users.noreply.github.com>
2023-10-02 19:18:58 -0700

    Delete Homework 1 directory

--- Homework 1/chatbot.py
+++
@@ -1,9 +0,0 @@
-# Homework 1 Part 2
-# Mason McLuckie
-#
-# Submitting Multiple Programs/ Hello User I am Chat Bot
-
-# chatbot.py
-print("Hello user!")
-print()
-print("I'm the", "ECS32A", "chatbot!\n")
--- Homework 1/welcome.py
+++
@@ -1,5 +0,0 @@
-# Homework 1 Problem/Part 1
-# Mason McLuckie
-#
-# Hello World Program
-print("Welcome to ECS32A")
